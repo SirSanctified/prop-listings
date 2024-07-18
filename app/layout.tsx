40adf2145757c9cbe67ec4e5281aec3211ec71d8
SirSanctified <mambambopritchard@gmail.com>
2024-07-18 11:09:01 +0200

    chore: Update layout and add Hero component
    - Update the layout to include a main container with auto margins and padding.
    - Add a new Hero component for the homepage.
    - Remove unused imports and files.

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,7 +59,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={lato.className}>
         <Nav />
-        {children}
+        <main className="mx-auto px-4">{children}</main>
         <Footer />
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>RealtyConnect</h1>
+      <Hero />
     </main>
   );
 }
